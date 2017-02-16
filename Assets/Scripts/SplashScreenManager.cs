00be94711c048de57d72754971eced622208b59d
Jamie Catania <catania.jamie@gmail.com>
2017-02-15 23:22:31 -0500

    Added splash screen image and stinger

---
+++ Assets/Scripts/SplashScreenManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashScreenManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("ChangeLevel", 3.0f);
+		Debug.Log ("New Level load: Start");
+	}
+
+	void ChangeLevel() {
+		Application.LoadLevel ("Start Menu");
+	}
+}
